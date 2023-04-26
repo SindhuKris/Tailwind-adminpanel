54339133014ca0e014112da3007549f6724fee42
Admin <admin@Admins-Air.phub.net.cable.rogers.com>
2023-04-26 00:31:16 -0400

    6-add-auth-with-sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,18 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  darkMode: false,
+  theme:{
+    extend:{
+      colors:{
+        gray:{
+          400: '#969798',
+          500: '#666666',
+          700: '#353535',
+          800: '#262626',
+          900: '#171818',
+        }
+      }
+    }
+  }
 }

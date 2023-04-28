e8175fe826127b29fbc673a24524e86a64d878ef
Admin <admin@Admins-Air.phub.net.cable.rogers.com>
2023-04-27 22:55:10 -0400

    listing in sidebar

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,10 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
- public primaryMenus=["Home","DashBoard", "Design", "Market&Sell", "Reporting", "Support"];
+ public primaryMenus=[
+  {
+    name: 'Home', 
+    icon: '',
+    subMenu: []
+  },
+  {
+    name:'Dashboard',
+    icon:'',
+    subMenu: []
+  },
+  {
+    name:'Content',
+    icon:'',
+    subMenu: ['Courses', 'Categories', 'Instructors', 'Video Library']
+  },
+  {
+    name:'Design',
+    icon:'',
+    subMenu: ['Black Design', 'Light Design', 'Material Design', 'Soft Design']
+  },
+  {
+    name:'Market & Sell',
+    icon:'',
+    subMenu: ['Marketing', 'Sales', 'Service', 'Operations']
+  },
+  {
+    name:'Reporting',
+    icon:'',
+    subMenu: ['Sales Overview', 'Sales Pipeline', 'Sales Trend with Forecast', 'Sales Target']
+  },
+  {
+    name:'Support',
+    icon:'',
+    subMenu: ['About Us', 'Contact Us', 'Customer Support']
+  },
+  
+
+ ];
+ hideList: boolean = false;
+
+ constructor() { }
+
+  toggleList(): void {
+    this.hideList = !this.hideList;
+  }
 
  public secondaryMenus=["Settings", "Notifications", "Apps"];
- public subMenus=["Courses", "Categories", "Instructors", "Video Library"]
+ public subMenus=[];
 
 
 }
